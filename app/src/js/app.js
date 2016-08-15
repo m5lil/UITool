591a72ba0dfaa8dce05e451e59532846ca57d99f
mkhlil1288 <mkhlil1288@gmail.com>
2016-08-15 05:27:05 +0200

    Second

---
+++ app/src/js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import './modules/helpers';
+
+// Import jQuery (if you need jquery);
+import $ from 'jquery';
+window.jQuery = $;
+console.log($.fn.jquery);
+
+import start from './modules/router';
+
+start();
